af069b63e2ab465727af277ed4c7c3ccacfbb855
GLADOS <glados@robotcasserole.org>
2019-01-08 20:52:24 -0600

    Beginning line following code

---
+++ Robotcode/RobotCode2019/src/main/java/frc/robot/lineFolower.java
@@ -0,0 +1,14 @@
+package frc.robot;
+
+public class lineFolower {
+
+    public boolean Sensor1;
+    public boolean Sensor2;
+    public boolean Sensor3;
+    public boolean Sensor4;
+    public boolean Sensor5;
+    public double leftSensorPosition_IN = 3;
+    public double rightSensorPosition_IN = -3;
+    public double midSensorPosition_IN = 0;
+
+}
