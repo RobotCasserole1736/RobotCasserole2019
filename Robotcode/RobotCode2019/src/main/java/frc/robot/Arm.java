8728841792a4642984a9beee9a67ea8293a62897
ADA <ada@robotcasserole.org>
2019-01-10 20:54:37 -0600

    Still not sure what ports for potentiometer

--- Robotcode/RobotCode2019/src/main/java/frc/robot/Arm.java
+++ Robotcode/RobotCode2019/src/main/java/frc/robot/Arm.java
@@ -1,7 +1,32 @@
 package frc.robot;
 
+import edu.wpi.first.wpilibj.DigitalSource;
+import edu.wpi.first.wpilibj.Encoder;
+import edu.wpi.first.wpilibj.AnalogPotentiometer;
+import edu.wpi.first.wpilibj.DigitalInput;
+
 public class Arm {
 
+    /////////Input Commands\\\\\\\\\\\
+    public double   desiredPreset;
+    
+    //////State Varibles\\\\\\\\\\\\
+    public double   desiredHeight = 0;
+    public double   curentHeight = 0;
+    public double   potUpVolt;
+    public double   potLowVolt;
+
+    /////////Limit Switches\\\\\\
+    public boolean topOfMotion = false;
+    public boolean bottomOfMotion = false; 
+    
+    
+    /////////Sensors\\\\\\\\\s
+    AnalogPotentiometer armPotPos;
+    Encoder armEncoder;
+
+
+    /////Initialization Code\\\\\\
     private static Arm  singularInstance = null;
 
     public static synchronized Arm getInstance() {
@@ -9,9 +34,59 @@ public class Arm {
 			singularInstance = new Arm();
         return singularInstance;
     }
-    
-    private Arm() {
 
+    private Arm() {
+        
+        armEncoder = new Encoder(0,1);
+        armPotPos = AnalogPotentiometer();
     } 
 
+    public enum armPos {
+        Top, Middle, Lower, Intake
+    }
+
+
+    public void sampleSensors() {
+
+    }
+
+    /////Use Sensor Data in Calculations\\\\\
+    public void update() {
+        
+        double convertVoltsToDeg(double voltage_in) {
+            return (x - LowerLimitVoltage) * (UpperLimitDegrees - LowerLimitDegrees) / (UpperLimitVoltage - LowerLimitVoltage) + LowerLimitDegrees;
+        }
+    }
+
+    public void setPositionCmd(armPos pos_in) {
+
+    }
+
+    public void setManualMovementCmd(double mov_cmd_in) {
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
 }
