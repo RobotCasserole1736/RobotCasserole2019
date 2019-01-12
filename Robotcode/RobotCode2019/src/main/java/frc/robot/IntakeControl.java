c94783c0c9357143f628af65c56c3e9d4d47bc58
BUTTERCUP <buttercup@robotcasserole.com>
2019-01-12 13:11:28 -0600

    created input control class

---
+++ Robotcode/RobotCode2019/src/main/java/frc/robot/IntakeControl.java
@@ -0,0 +1,70 @@
+package frc.robot;
+
+/*
+ *******************************************************************************************
+ * Copyright (C) 2019 FRC Team 1736 Robot Casserole - www.robotcasserole.org
+ *******************************************************************************************
+ *
+ * This software is released under the MIT Licence - see the license.txt
+ *  file in the root of this repo.
+ *
+ * Non-legally-binding statement from Team 1736:
+ *  Thank you for taking the time to read through our software! We hope you
+ *   find it educational and informative! 
+ *  Please feel free to snag our software for your own use in whatever project
+ *   you have going on right now! We'd love to be able to help out! Shoot us 
+ *   any questions you may have, all our contact info should be on our website
+ *   (listed above).
+ *  If you happen to end up using our software to make money, that is wonderful!
+ *   Robot Casserole is always looking for more sponsors, so we'd be very appreciative
+ *   if you would consider donating to our club to help further STEM education.
+ */
+
+public class IntakeControl {
+	// You will want to rename all instances of "EmptyClass" with your actual class name and "empty" with a variable name
+    private static IntakeControl empty = null;
+
+	public static synchronized IntakeControl getInstance() {
+		if(empty == null)
+			empty = new IntakeControl();
+		return empty;
+    }
+
+    //Intake positions that can be requested
+    public enum IntakePos {
+        Extend(0), Retract(1);
+
+        public final int value;
+
+		private IntakePos(int value) {
+            this.value = value;
+        }
+	}
+	
+	
+    //Intake positions that can be requested
+    public enum IntakePos {
+        Extend(0), Retract(1);
+
+        public final int value;
+
+		private IntakePos(int value) {
+            this.value = value;
+        }
+    }
+
+
+	// This is the private constructor that will be called once by getInstance() and it should instantiate anything that will be required by the class
+
+
+	//This is a dummy variable that you will probably want to delete
+	private boolean testIsEverythingOK = false;
+
+	// This is a dummy method that you will probably want to delete
+	public void TestCommand() {
+		if(testIsEverythingOK)
+			System.out.println("OK");
+		else
+			System.out.println("NOT OK");
+	}
+}
