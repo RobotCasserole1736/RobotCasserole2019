bdaef5927c97b0da152c0e5b1441605c05881ae6
BUTTERCUP <buttercup@robotcasserole.com>
2019-01-12 15:40:29 -0600

    finish intake control

--- Robotcode/RobotCode2019/src/main/java/frc/robot/IntakeControl.java
+++ Robotcode/RobotCode2019/src/main/java/frc/robot/IntakeControl.java
@@ -1,5 +1,9 @@
 package frc.robot;
 
+import edu.wpi.first.wpilibj.DigitalInput;
+import edu.wpi.first.wpilibj.Solenoid;
+import edu.wpi.first.wpilibj.Spark;
+
 /*
  *******************************************************************************************
  * Copyright (C) 2019 FRC Team 1736 Robot Casserole - www.robotcasserole.org
@@ -20,15 +24,28 @@ package frc.robot;
  *   if you would consider donating to our club to help further STEM education.
  */
 
+
+
 public class IntakeControl {
+
+	DigitalInput ballInIntake;
+	Spark intakeMotor;
+	Solenoid intakeArmBar;
+
 	// You will want to rename all instances of "EmptyClass" with your actual class name and "empty" with a variable name
-    private static IntakeControl empty = null;
+	private static IntakeControl empty = null;
 
 	public static synchronized IntakeControl getInstance() {
 		if(empty == null)
 			empty = new IntakeControl();
 		return empty;
-    }
+	}
+	
+	private IntakeControl(){
+		ballInIntake = new DigitalInput(1);
+		intakeMotor = new Spark(2);
+		intakeArmBar = new Solenoid(5);
+	}
 
     //Intake positions that can be requested
     public enum IntakePos {
@@ -43,15 +60,64 @@ public class IntakeControl {
 	
 	
     //Intake positions that can be requested
-    public enum IntakePos {
-        Extend(0), Retract(1);
+    public enum IntakeSpd {
+        Stop(0), Intake(1), Eject(2);
 
         public final int value;
 
-		private IntakePos(int value) {
+		private IntakeSpd(int value) {
             this.value = value;
         }
-    }
+	}
+
+	//Start with intake retracted and stopped
+	private IntakePos intakePosCmd = IntakePos.Retract;
+	private IntakeSpd intakeSpdCmd = IntakeSpd.Stop;
+
+	public void setPositionCmd(IntakePos pos_in){
+		if(pos_in == IntakePos.Retract){
+			intakePosCmd = IntakePos.Retract;
+		}else if(pos_in == IntakePos.Extend){
+			intakePosCmd = IntakePos.Extend;
+		}
+	}
+
+	public void setSpeedCmd(IntakeSpd spd_in){
+		if(spd_in == IntakeSpd.Stop){
+			intakeSpdCmd = IntakeSpd.Stop;
+		}else if(spd_in == IntakeSpd.Intake){
+			intakeSpdCmd = IntakeSpd.Intake;
+		}else if(spd_in == IntakeSpd.Eject){
+			intakeSpdCmd = IntakeSpd.Eject;
+		}
+
+	}
+
+	public void update(){
+		//Pneumatic arm bar thingy control
+		if(intakePosCmd == IntakePos.Retract){
+			intakeArmBar.set(false);
+		}else if(intakePosCmd == IntakePos.Extend){
+			intakeArmBar.set(true);
+		}else{ //if for some reason it is confused, pick up the intake arm bar thingy
+			intakeArmBar.set(false);
+		}
+
+		//Intake motor control
+		if((ballInIntake.get() == true) && (intakeSpdCmd == IntakeSpd.Intake)){ //If we got a ball, don't Intake
+			intakeMotor.set(0);
+		}else if((ballInIntake.get() == false) && (intakeSpdCmd == IntakeSpd.Intake)){ //If we don't, Intake
+			intakeMotor.set(1);
+		}else if(intakeSpdCmd == IntakeSpd.Stop){ //Whether we have a ball or not, we can Stop and Eject
+			intakeMotor.set(0);
+		}else if(intakeSpdCmd == IntakeSpd.Eject){
+			intakeMotor.set(-1);
+		}else{ //If for some reason it is confused, don't run the intake
+			intakeMotor.set(0);
+		}
+	}
+
+
 
 
 	// This is the private constructor that will be called once by getInstance() and it should instantiate anything that will be required by the class
