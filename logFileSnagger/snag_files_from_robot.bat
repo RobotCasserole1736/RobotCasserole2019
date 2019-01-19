2a3004fa468cedb7b188db24bb720e3dbddbf368
joshua <joshua@1736.org>
2019-01-19 11:06:49 -0600

    updated file snagger to work with diff versions of python

--- logFileSnagger/snag_files_from_robot.bat
+++ logFileSnagger/snag_files_from_robot.bat
@@ -1,4 +1,12 @@
 @echo off
-C:\Python34\python.exe file_snagger\snag_files.py
+if exist c:\Python34\python.exe (
+	C:\Python34\python.exe file_snagger\snag_files.py
+) else (
+	if exist c:\Python37\python.exe (
+		C:\Python37\python.exe file_snagger\snag_files.py
+	) else (
+		python file_snagger\snag_files.py
+	)
+)
 
 pause 
