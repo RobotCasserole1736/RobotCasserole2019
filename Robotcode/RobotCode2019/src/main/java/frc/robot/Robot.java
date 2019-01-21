cc8de09651d31fcfeaf6448369f9cf2c91916dd9
Chris Gerth <chrisgerth010592@gmail.com>
2019-01-21 08:36:22 -0600

    Comment fixup

--- Robotcode/RobotCode2019/src/main/java/frc/robot/Robot.java
+++ Robotcode/RobotCode2019/src/main/java/frc/robot/Robot.java
@@ -1,8 +1,8 @@
 /*----------------------------------------------------------------------------*/
-/* Copyright (c) 2017-2018 FIRST. All Rights Reserved.                                                */
-/* Open Source Software - may be modified and shared by FRC teams. The code     */
+/* Copyright (c) 2017-2018 FIRST. All Rights Reserved.                        */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
 /* must be accompanied by the FIRST BSD license file in the root directory of */
-/* the project.                                                                                                                             */
+/* the project.                                                               */
 /*----------------------------------------------------------------------------*/
 
 package frc.robot;
