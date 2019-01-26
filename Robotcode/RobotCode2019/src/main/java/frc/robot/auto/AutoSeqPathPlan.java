661d2902616fecf4bb4ad9ffd6d4d864e6ae79dc
Chris Gerth <chrisgerth010592@gmail.com>
2019-01-26 16:14:52 -0600

    build fixup

--- Robotcode/RobotCode2019/src/main/java/frc/robot/auto/AutoSeqPathPlan.java
+++ Robotcode/RobotCode2019/src/main/java/frc/robot/auto/AutoSeqPathPlan.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 
 import frc.lib.AutoSequencer.AutoEvent;
 import frc.lib.PathPlanner.FalconPathPlanner;
+import frc.robot.RobotConstants;
 
 /*
  *******************************************************************************************
