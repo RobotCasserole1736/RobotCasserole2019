04b9de3177c6aa0ce05062792fe80c38ad5fb86e
Chris Gerth <chrisgerth010592@gmail.com>
2019-02-16 15:30:11 -0600

    Fixed bug with intake reporting it was at desired position too early.

--- Robotcode/RobotCode2019/src/main/java/frc/robot/IntakeControl.java
+++ Robotcode/RobotCode2019/src/main/java/frc/robot/IntakeControl.java
@@ -185,6 +185,7 @@ public class IntakeControl{
 
     //Start with intake retracted and stopped
     private IntakePos intakePosCmd = IntakePos.Retract;
+    private IntakePos prevIntakePosCmd = IntakePos.Retract;
     private IntakeSpd intakeSpdCmd = IntakeSpd.Stop;
 
     public void setPositionCmd(IntakePos posIn){
@@ -270,7 +271,7 @@ public class IntakeControl{
             ballDetected = false;
 
         } else {
-    
+
             if(MatchState.getInstance().GetPeriod() != MatchState.Period.OperatorControl &&
                MatchState.getInstance().GetPeriod() != MatchState.Period.Autonomous) {
                 //Start intake within frame perimiter and in safe state
@@ -292,6 +293,11 @@ public class IntakeControl{
                 //Don't change the setpoint otherwise.
             }
 
+            if(intakePosCmd != prevIntakePosCmd){
+                rightOnTargetDbnc.resetCounters();
+                leftOnTargetDbnc.resetCounters();
+            }
+
             //Debounce whether we're at the correct position or not.
             rightOnTarget = rightOnTargetDbnc.BelowDebounce(Math.abs(intakeRightArmMotor.getCurError()));
             leftOnTarget  = leftOnTargetDbnc.BelowDebounce(Math.abs(intakeLeftArmMotor.getCurError()));
@@ -311,6 +317,8 @@ public class IntakeControl{
             }
     
             intakeMotor.set(-1*intakeMotorCmd); //motor is mechanically inverted
+
+            prevIntakePosCmd = intakePosCmd;
     
             double sampleTimeMS = LoopTiming.getInstance().getLoopStartTimeSec() * 1000.0;
             motorSpeedCmdSig.addSample(sampleTimeMS, intakeMotorCmd);
