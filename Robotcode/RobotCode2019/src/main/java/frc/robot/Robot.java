5d28319e34d35861315c658a4ef1ac284a6cf024
Chris Gerth <chrisgerth010592@gmail.com>
2019-02-26 20:16:00 -0600

    Force climber manual movemnet back to false during match periodic

--- Robotcode/RobotCode2019/src/main/java/frc/robot/Robot.java
+++ Robotcode/RobotCode2019/src/main/java/frc/robot/Robot.java
@@ -320,10 +320,12 @@ public class Robot extends TimedRobot {
             poseCalc.setRightMotorSpeed(drivetrain.getRightWheelSpeedRPM());
             poseCalc.update();
 
+            climber.setManualMovement(false);
+            climber.update();
+
             /* Update Other subsytems */
             ledController.update();
             pneumaticsControl.update();
-            climber.update();
             telemetryUpdate();
             
             /*Update CrashTracker*/
