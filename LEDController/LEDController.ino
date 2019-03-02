a4ce36b1a28ea631a557783453658298699f3bb9
Chris Gerth <chrisgerth010592@gmail.com>
2019-03-02 17:27:44 -0600

    Removed a few delays from the arduino code

--- LEDController/LEDController.ino
+++ LEDController/LEDController.ino
@@ -1,5 +1,4 @@
 #include <FastLED.h>
-#include <Streaming.h>
 
 #define DATA_PIN    6
 //#define CLK_PIN   4
@@ -11,53 +10,55 @@ CRGB leds[NUM_LEDS];
 #define BRIGHTNESS          96
 #define FRAMES_PER_SECOND  120
 
-int pin = 4;
+int rioControlPin = 4;
 int PWMV;
 int patternNumber = 0;
 String patternName = "???";
 int currentCount;
 
 void setup() {
+  Serial.begin(57600);
+  Serial.println("Init Start");
   delay(3000); // 3 second delay for recovery
 
   // tell FastLED about the LED strip configuration
   FastLED.addLeds<LED_TYPE, DATA_PIN, COLOR_ORDER>(leds, NUM_LEDS).setCorrection(TypicalLEDStrip);
-  //FastLED.addLeds<LED_TYPE,DATA_PIN,CLK_PIN,COLOR_ORDER>(leds, NUM_LEDS).setCorrection(TypicalLEDStrip);
-
+  
   // set master brightness control
   FastLED.setBrightness(BRIGHTNESS);
 
-  Serial.begin(57600);
-  pinMode(pin, INPUT);
+
+  pinMode(rioControlPin, INPUT);
+
+  Serial.println("Init Complete");
 }
 
 uint8_t gHue = 0; // rotating "base color" used by many of the patterns
 
 void readPWM() {
-  PWMV = pulseIn(pin, HIGH, 50000);
+  PWMV = pulseIn(rioControlPin, HIGH, 50000);
 }
 
 void loop()
 {
   if ((PWMV >= -50) && (PWMV <= 50))
   {
-    colorWipe(0xff, 0x00, 0x00, 50);
-    colorWipe(0xff, 0xff, 0xff, 50);
+    EVERY_N_MILLISECONDS(50){colorWipe();}
   }
 
   if ((PWMV >= 950) && (PWMV <= 1050))
   {
-    RunningLights(0xff, 0x00, 0x00, 50);
+    RunningLights(0xff, 0x00, 0x00);
   }
 
   if ((PWMV >= 1200) && (PWMV <= 1300))
   {
-    RunningLights(0xff, 0xff, 0xff, 50);
+    RunningLights(0xff, 0xff, 0xff);
   }
 
   if ((PWMV >= 1450) && (PWMV <= 1550))
   {
-    Strobe(0xff, 0x00, 0x00, 10, 50, 1000);
+    Strobe(0xff, 0x00, 0x00);
   }
 
   if ((PWMV >= 1700) && (PWMV <= 1800))
@@ -90,12 +91,23 @@ void loop()
 // Pattern: Color Wipe
 //**************************************************************
 
-void colorWipe(byte red, byte green, byte blue, int SpeedDelay) {
-  for (uint16_t i = 0; i < NUM_LEDS; i++) {
-    setPixel(i, red, green, blue);
-    showStrip();
-    delay(SpeedDelay);
+boolean colorWipeIsRed = false;
+unsigned int colorWipePixelCounter = 0;
+
+void colorWipe() {
+  Serial.println("Running Color Wipe");
+  if(colorWipeIsRed) {
+    setPixel(colorWipePixelCounter, 0xff, 0x00, 0x00);
+  } else {
+    setPixel(colorWipePixelCounter, 0xff, 0xff, 0xff);
   }
+
+  colorWipePixelCounter++;
+  if(colorWipePixelCounter >= NUM_LEDS){
+    colorWipePixelCounter = 0;
+    colorWipeIsRed = !colorWipeIsRed;
+  }
+  
 }
 
 //**************************************************************
@@ -104,8 +116,10 @@ void colorWipe(byte red, byte green, byte blue, int SpeedDelay) {
 
 void CylonBounce(byte red, byte green, byte blue, int EyeSize, int SpeedDelay, int ReturnDelay) {
 
+  Serial.println("Running Cylon Bounce");
   for (int i = 0; i < NUM_LEDS - EyeSize - 2; i++) {
     setAll(0, 0, 0);
+    
     setPixel(i, red / 10, green / 10, blue / 10);
     for (int j = 1; j <= EyeSize; j++) {
       setPixel(i + j, red, green, blue);
@@ -119,6 +133,7 @@ void CylonBounce(byte red, byte green, byte blue, int EyeSize, int SpeedDelay, i
 
   for (int i = NUM_LEDS - EyeSize - 2; i > 0; i--) {
     setAll(0, 0, 0);
+    
     setPixel(i, red / 10, green / 10, blue / 10);
     for (int j = 1; j <= EyeSize; j++) {
       setPixel(i + j, red, green, blue);
@@ -136,6 +151,7 @@ void CylonBounce(byte red, byte green, byte blue, int EyeSize, int SpeedDelay, i
 //**************************************************************
 
 void meteorRain(byte red, byte green, byte blue, byte meteorSize, byte meteorTrailDecay, boolean meteorRandomDecay, int SpeedDelay) {
+  Serial.println("Running Meteor");
   setAll(0, 0, 0);
 
   for (int i = 0; i < NUM_LEDS + NUM_LEDS; i++) {
@@ -147,9 +163,19 @@ void meteorRain(byte red, byte green, byte blue, byte meteorSize, byte meteorTra
     }
     // draw meteor
     for (int j = 0; j < meteorSize; j++) {
+      
       if ( ( i - j < NUM_LEDS) && (i - j >= 0) ) {
         setPixel(i - j, red, green, blue);
       }
+      
+      if( ( j == (meteorSize - 1) ) && ( i - j < NUM_LEDS) && (i - j >= 0) ) {
+        if(random(10) > 5){
+          Serial.print("setting LED ");
+          Serial.println(i-j);
+          setPixel(i - j, 0xff, 0x00, 0x00);
+        }
+      }
+      
     }
     showStrip();
     delay(SpeedDelay);
@@ -164,29 +190,35 @@ void fadeToBlack(int ledNo, byte fadeValue) {
 // Pattern: Running Lights
 //**************************************************************
 int Position = 0;
-void RunningLights(byte red, byte green, byte blue, int WaveDelay) {
-
+void RunningLights(byte red, byte green, byte blue) {
+  Serial.println("Running Running Lights Wipe");
 
   Position++; // = 0; //Position + Rate;
   for (int i = 0; i < NUM_LEDS; i++) {
     setPixel(i, ((sin(i + Position / 2) * 127 + 128) / 255)*red,
-             ((sin(i + Position / 2) * 127 + 128) / 255)*green,
-             ((sin(i + Position / 2) * 127 + 128) / 255)*blue);
+                ((sin(i + Position / 2) * 127 + 128) / 255)*green,
+                ((sin(i + Position / 2) * 127 + 128) / 255)*blue);
   }
 }
 
 //**************************************************************
 // Pattern: Strobe
 //**************************************************************
+int strobeCounter = 0;
+boolean strobeFlashState;
+void Strobe(byte red, byte green, byte blue) {
+  Serial.println("Running Strobe");
+  strobeCounter = strobeCounter + 1;
 
-void Strobe(byte red, byte green, byte blue, int StrobeCount, int FlashDelay, int EndPause) {
-  for (int j = 0; j < StrobeCount; j++) {
+  if(strobeCounter > 5){
+    strobeCounter = 0;
+    strobeFlashState = !strobeFlashState;
+  }
+
+  if(strobeFlashState){
     setAll(red, green, blue);
-    showStrip();
-    delay(FlashDelay);
-    setAll(0, 0, 0);
-    showStrip();
-    delay(FlashDelay);
+  } else {
+    setAll(0, 0, 0); 
   }
 }
 
